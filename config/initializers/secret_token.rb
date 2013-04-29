13a1153f5fbc82f869f4a06e4958a0b01e6f3de3
chana <chana.black@gmail.com>
2013-04-29 16:23:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KataChutes::Application.config.secret_token = '84989b2ff5807e579abf0030e3dd3ccdfbd7ede03d43f2eec26d2182e9809aa7e341aa53603d66919eb61f7e0052da99a8e3352daad91d4d423c32d175c2baa4'
